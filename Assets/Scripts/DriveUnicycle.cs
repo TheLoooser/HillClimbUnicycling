ed48c43adc66a30138246e6af8963c0b83f3e190
TheLoooser <ottifant99@hotmail.com>
2024-04-21 12:53:06 +0200

    Implement ground hugging

--- Assets/Scripts/DriveUnicycle.cs
+++ Assets/Scripts/DriveUnicycle.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DriveUnicycle : MonoBehaviour
@@ -7,6 +5,11 @@ public class DriveUnicycle : MonoBehaviour
     [SerializeField] private Rigidbody2D _tireRB;
     [SerializeField] private float _speed = 150f;
     private float _moveInput;
+
+    private RaycastHit2D hit;
+    public Transform raycastPoint;
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +22,22 @@ public class DriveUnicycle : MonoBehaviour
     {
         _moveInput = Input.GetAxisRaw("Horizontal");
 
+        Vector2 forward = transform.TransformDirection(Vector2.right) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
     }
 
     private void FixedUpdate()
     {
         _tireRB.AddTorque(-_moveInput * _speed * Time.fixedDeltaTime);
+
+        hit = Physics2D.Raycast(raycastPoint.position, Vector2.down);
+        Debug.Log(hit.transform);
+        Debug.Log(hit.collider);
+        Debug.Log(hit.distance);
+
+        transform.position = new Vector2(transform.position.x, transform.position.y - hit.distance + 1.02f);
+        Debug.DrawRay(new Vector3(transform.position.x, transform.position.y, 0), transform.TransformDirection(Vector2.down) * 10, Color.yellow);
+        
     }
 }
